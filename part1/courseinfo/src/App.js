54fa7f9d24c62ff98dbe6e3b8d7885757d32054b
ibafly <copppe@gmail.com>
2021-11-02 22:18:15 +0800

    part1 1.5 exercise done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,41 +3,35 @@ import React from "react"
 const Header = props => <h1>{props.text}</h1>
 
 const Content = props =>
-  props.parts.map(i => <Part part={i.part} exercise={i.exercise} />)
+  props.parts.map(i => <Part name={i.name} exercises={i.exercises} />)
 
 const Part = props => (
   <p>
-    {props.part} {props.exercise}
+    {props.name} {props.exercises}
   </p>
 )
 
 const Total = props => (
   <p>
     Number of exercises{" "}
-    {props.exercises.reduce((partial_sum, i) => partial_sum + i, 0)}
+    {props.parts.reduce((partial_sum, i) => partial_sum + i.exercises, 0)}
   </p>
 )
 
 const App = () => {
-  const course = "Half Stack application development"
-  const part1 = "Fundamentals of React"
-  const exercise1 = 10
-  const part2 = "Using props to passing data"
-  const exercise2 = 7
-  const part3 = "State of an component"
-  const exercise3 = 14
-
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to passing data", exercises: 7 },
+      { name: "State of an component", exercises: 14 },
+    ],
+  }
   return (
     <div>
-      <Header text={course}></Header>
-      <Content
-        parts={[
-          { part: part1, exercise: exercise1 },
-          { part: part2, exercise: exercise2 },
-          { part: part3, exercise: exercise3 },
-        ]}
-      ></Content>
-      <Total exercises={[exercise1, exercise2, exercise3]}></Total>
+      <Header text={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
     </div>
   )
 }

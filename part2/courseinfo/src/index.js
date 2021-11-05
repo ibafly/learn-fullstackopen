b1a20cc419c8d0ea3dc366ab1128b73356a355ba
ibafly <copppe@gmail.com>
2021-11-05 19:25:08 +0800

    part2 2.5 separate module done

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,87 +1,50 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import App from "./App"
 
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+const courses = [
+  {
+    name: "Half Stack application development",
+    id: 1,
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  },
+  {
+    name: "Node.js",
+    id: 2,
+    parts: [
+      {
+        name: "Routing",
+        exercises: 3,
+        id: 1,
+      },
+      {
+        name: "Middlewares",
+        exercises: 7,
+        id: 2,
+      },
+    ],
+  },
+]
 
-const Header = ({ name }) => {
-  return <h1>{name}</h1>
-}
-
-const Content = ({ parts }) => {
-  return parts.map(part => (
-    <Part key={part.id} name={part.name} exercises={part.exercises} />
-  ))
-}
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  )
-}
-
-const Total = ({ parts }) => {
-  // const sum = parts.reduce((iSum, part, i, arr) => iSum + part.exercises, 0)
-  const sum = parts.reduce((iSum, part) => iSum + part.exercises, 0)
-  return <p>Number of exercises {sum}</p>
-}
-
-const App = () => {
-  const courses = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ]
-
-  return courses.map(course => <Course course={course} />)
-}
-
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App courses={courses} />, document.getElementById("root"))

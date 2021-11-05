955e733d2a62f21693b345beeea2086316a7e44c
ibafly <copppe@gmail.com>
2021-11-05 13:42:29 +0800

    part1 1.14* anecdotes step3 done

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react"
 
+const Title = ({ heading, text }) => {
+  const TagName = `${heading}`
+  return <TagName>{text}</TagName>
+}
+
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const VoteStat = ({ text }) => <p>has {text} votes</p>
@@ -18,23 +23,33 @@ const App = () => {
   const [selected, setSelected] = useState(0)
   const [voted, setVoted] = useState(new Uint8Array(anecdotes.length))
 
-  const pickAnecdote = () => setSelected(getRandomInt(anecdotes.length))
+  const pickAnecdote = () => setSelected(getDistinctRandomInt(anecdotes.length))
   const voteAnecdote = () => {
     let votedCopy = [...voted]
     votedCopy[selected] = votedCopy[selected] + 1
     setVoted(votedCopy)
   }
 
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max)
+  function getDistinctRandomInt(max) {
+    let rand = Math.floor(Math.random() * max)
+    return rand === selected ? getDistinctRandomInt(max) : rand
+  }
+  function getIndexOfMax(arr) {
+    return arr.indexOf(Math.max(...arr))
+    // another way to get index of largest number in array:
+    // arr.reduce((iMax, val, i, arr) => (val > arr[iMax] ? i : iMax), 0)
   }
 
   return (
     <div>
+      <Title heading="h1" text="Anecdote of the day" />
       <p>{anecdotes[selected]}</p>
       <VoteStat text={voted[selected]} />
       <Button onClick={voteAnecdote} text="vote" />
       <Button onClick={pickAnecdote} text="next anecdote" />
+
+      <Title heading="h2" text="Anecdote with most votes" />
+      <p>{anecdotes[getIndexOfMax(voted)]}</p>
     </div>
   )
 }

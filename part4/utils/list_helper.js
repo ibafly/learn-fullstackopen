fd329749f82f428ed7c8b4a9537ff8bab3f4a380
ibafly <copppe@gmail.com>
2021-12-19 13:07:38 +0800

    part4 4.4 helper functions and unit tests step2 done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,13 @@
-const dummy = blog => {
+const dummy = blogs => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = blogs => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
+module.exports = { dummy, totalLikes }

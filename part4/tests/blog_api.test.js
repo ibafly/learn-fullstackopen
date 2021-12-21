17c6e6ee51d34ac8da12aef5d10930a715a057ee
ibafly <copppe@gmail.com>
2021-12-21 14:13:32 +0800

    part4 4.9* blog list tests step2 done

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -11,12 +11,17 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 }, 100000)
 
-test("blogs are returned as JSON and there are 6 blogs initially", async () => {
+test("blogs are returned as JSON && there are 6 blogs initially && id property is named 'id'", async () => {
   const res = await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/)
+
   expect(res.body).toHaveLength(6)
+
+  for (blogObj of res.body) {
+    expect(blogObj.id).toBeDefined()
+  }
 }, 100000) // Jest default test timeout is 5000ms
 
 afterAll(() => {

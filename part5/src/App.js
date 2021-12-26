061c03858969c44e238eed0d5c69dd69693076ea
ibafly <copppe@gmail.com>
2021-12-26 19:46:25 +0800

    part5 5.2 blog list frontend step2 done

--- part5/src/App.js
+++ part5/src/App.js
@@ -11,6 +11,21 @@ const App = () => {
   const [password, setPassword] = useState("")
   const [user, setUser] = useState(null)
 
+  useEffect(() => {
+    blogService
+      .getAll()
+      .then(blogs => setBlogs(blogs))
+      .catch(err => {
+        console.log(err.response.data.error)
+      })
+  }, [])
+
+  useEffect(() => {
+    const loggedUser = JSON.parse(window.localStorage.getItem("loggedUser"))
+    loggedUser && setUser(loggedUser)
+    console.log(loggedUser)
+  }, [])
+
   const followUsernameInput = event => {
     setUsername(event.target.value)
   }
@@ -23,6 +38,7 @@ const App = () => {
     try {
       const user = await loginService.login({ username, password })
       setUser(user)
+      window.localStorage.setItem("loggedUser", JSON.stringify(user))
       setUsername("")
       setPassword("")
     } catch (excep) {
@@ -33,14 +49,10 @@ const App = () => {
     }
   }
 
-  useEffect(() => {
-    blogService
-      .getAll()
-      .then(blogs => setBlogs(blogs))
-      .catch(err => {
-        console.log(err.response.data.error)
-      })
-  }, [])
+  const handleLogout = event => {
+    window.localStorage.removeItem("loggedUser")
+    setUser(null)
+  }
 
   const loginSection = () => {
     return (
@@ -61,7 +73,10 @@ const App = () => {
     return (
       <div>
         <h2>blogs</h2>
-        <h3>{user ? user.name : ""} logged in</h3>
+        <h3>
+          {user ? user.name : ""} logged in
+          <button onClick={handleLogout}>logout</button>
+        </h3>
         {blogs.map(blog => (
           <Blog key={blog.id} blog={blog} />
         ))}

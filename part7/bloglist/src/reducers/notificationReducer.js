1e2c481e0751cb14c9bb3ec31079d9831a784676
ibafly <copppe@gmail.com>
2022-01-14 23:47:56 +0800

    part7 7.9 redux step1 done

---
+++ part7/bloglist/src/reducers/notificationReducer.js
@@ -0,0 +1,37 @@
+const reducer = (state = "", action) => {
+  switch (action.type) {
+    case "SET":
+      return action.content
+    case "CLEAR":
+      return ""
+    default:
+      return state
+  }
+}
+
+let lastTimeoutId = undefined
+
+export const setMsg = (content, duration) => {
+  return async dispatch => {
+    dispatch({
+      type: "SET",
+      content,
+    })
+
+    if (typeof lastTimeoutId === "number") {
+      clearTimeout(lastTimeoutId) // cancel the last timeout fn to avoid message be cleared by last near click (timeout fn)
+    }
+
+    lastTimeoutId = await setTimeout(() => {
+      dispatch({
+        type: "CLEAR",
+      })
+    }, duration * 1000)
+  }
+}
+export const clearMsg = () => {
+  return {
+    type: "CLEAR",
+  }
+}
+export default reducer

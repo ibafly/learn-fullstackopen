fa5ee26469aa322ba2c966a951e72a7dddf2693c
ibafly <copppe@gmail.com>
2022-01-18 12:13:32 +0800

    part7 7.16 navigation done

--- part7/bloglist/src/components/Header.js
+++ part7/bloglist/src/components/Header.js
@@ -1,15 +1,23 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Header = ({ user, opAfterLogoutBtnOnClick }) => {
+  const style = {
+    background: "gray",
+  }
   return (
     <div>
+      <div style={style}>
+        <Link to="/blogs">blogs</Link>&nbsp;
+        <Link to="/users">users</Link>&nbsp;
+        {user && (
+          <span>
+            {user.name} logged in
+            <button onClick={opAfterLogoutBtnOnClick}>logout</button>
+          </span>
+        )}
+      </div>
       <h2>blogs</h2>
-      {user && (
-        <div>
-          {user.name} logged in <br />
-          <button onClick={opAfterLogoutBtnOnClick}>logout</button>
-        </div>
-      )}
     </div>
   )
 }

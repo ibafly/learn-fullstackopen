7c376722f761e8bcd57a393947816ddfc0220ae2
ibafly <copppe@gmail.com>
2022-02-14 19:11:46 +0800

    part9 9.10 patientor backend step3 done

--- part9/patientor/index.ts
+++ part9/patientor/index.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
-const app = express();
-app.use(express.json());
-app.use(cors());
+import diagnosesRouter from "./src/routes/diagnoses";
 
 const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
